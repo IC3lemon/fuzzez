71ee89e47fcb0c392aa69a654ad5544739f6db86
ash_n <111520157+oxo-crab@users.noreply.github.com>
2024-04-06 05:23:33 +0530

    better dependency.py

--- fuzzez/dependency.py
+++ fuzzez/dependency.py
@@ -3,12 +3,13 @@ import os
 
 def compare_directories(dir1, dir2):
     # Walk through the directory trees and compare files
+    print("comparing.....")
     for dirpath, dirnames, filenames in os.walk(dir1):
         for filename in filenames:
             file1 = os.path.join(dirpath, filename)
             file2 = os.path.join(dir2, os.path.relpath(file1, dir1))
             if not os.path.exists(file2):
-                print(f"File {file2} does not exist in directory {dir2}")
+                print(f"File {filename} does not exist in directory {dir2}")
             elif not filecmp.cmp(file1, file2, shallow=False):
                 print(f"Differences found in file: {file1}")
                 
@@ -32,12 +33,12 @@ if __name__ == "__main__":
     
     
     ver = ver("/home/crabsnk/Desktop/linux/Makefile")
-    print(ver)
-    os.chdir("{folder_path}")
+    print("version is:",ver)
+    os.chdir("/home/crabsnk/Desktop/linux")
     os.system("pwd")
     os.system('curl -O  mirrors.edge.kernel.org/pub/linux/kernel/{}/{}'.format(str('v'+ver[0]+'.x'),'linux-'+ver+'.tar.gz'))
     os.system("tar -xf {a}".format(a = 'linux-'+ver+'.tar.gz'))
-    directory1 = "home/crabsnk/linux/{}".format('linux-'+ver+'.tar.gz')
-    directory2 = "home/crabsnk/Desktop/linux/linux-6.8.3"
+    directory1 = "/home/crabsnk/Desktop/linux/{}".format('linux-'+ver)
+    directory2 = "/home/crabsnk/Desktop/linux/testcase"
 
     compare_directories(directory1, directory2) 
